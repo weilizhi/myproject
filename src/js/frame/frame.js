8f05bb2db49f82ba49510df88300885db06140d8
wlz <674501835@qq.com>
2019-10-23 19:02:03 +0800

    完成头部逻辑

--- src/js/frame/frame.js
+++ src/js/frame/frame.js
@@ -1,12 +1,15 @@
+import move from "./move/move"
 window.onload = function () {
-  var liNodes = document.querySelectorAll('html body #app .head .headMain .nav .item')
+  var liNodes = document.querySelectorAll('html body #app .head .headMain .nav .item');
+  var homeNode=liNodes[0];
+  var arrow=document.querySelector('html body #app .head .headMain .arrow');
+  //箭头图标的初始化配置
+  arrow.style.left= (homeNode.offsetLeft+(homeNode.offsetWidth/2)-(arrow.offsetWidth/2)) +'px';
   for (var i = 0; i < liNodes.length; i++) {
-    (function () {
+    liNodes[i].index=i;
+    (function (i) {
       liNodes[i].onclick = function () {
-        for (var j = 0; j < liNodes.length; j++) {
-          liNodes[j].classList.remove('active')
-        }
-        liNodes[i].classList.add('active');
+        move(this.index)
       }
     })(i)
 
